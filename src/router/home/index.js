2d946c3ecfd103c05e036c9f5a1c6b78da4d75df
heliang6 <3052618726@qq.com>
2019-11-14 21:35:20 +0800

    第二次

--- src/router/home/index.js
+++ src/router/home/index.js
@@ -1,6 +1,5 @@
 export default{
-    path:"./home",
+    path:"/home",
     component:()=>import("views/home"),
     name:"home"
-
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,28 +1,41 @@
 <template>
-  <div>
-  <router-view></router-view>
-    
-    <h2>home</h2>
-  </div>
+<div class="box">
+  <swiper :options="swiperOption">
+    <swiper-slide>
+      <img src="//media.china-sss.com/img/M00/05/48/wKjFbF2jzuaAWWNYAAKaQsTlCEg025.jpg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="//media.china-sss.com/img/M00/05/55/wKjFbF248vmAF4KjAAHwsOks1nA489.jpg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="//media.china-sss.com/img/M00/05/09/wKjFbF0-1dGAGTFkAAKMcwtb2y4372.jpg" alt="">
+    </swiper-slide>
+  </swiper>
+      <input type="text" class="searchIpt">
+
+
+</div>
 </template>
 <script>
-
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+ 
 export default {
-   name:"home",
-
- data(){
-     return{
-       
-     }
-   
-   },
-   methods:{
-   }
-
-} 
-</script>
+  components: {
+    swiper,
+    swiperSlide
+  }
+}</script>
 <style scoped>
-  
+.searchIpt{
+  width: 6rem;
+  height: .4rem;
+  position: absolute;
+  top: 3rem;
+  z-index: 99;
+  left: .7rem;
+  border-radius: .2rem;
+  border: 1px solid red;
+}
 </style>
 
 
